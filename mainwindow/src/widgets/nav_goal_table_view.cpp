82212d3d03ee4ff0f7f1bdd42d6aef5ebb58ba86
chengyangkj <chengyangkj@outlook.com>
2024-01-23 22:44:30 +0800

    nav goal list

--- mainwindow/src/widgets/nav_goal_table_view.cpp
+++ mainwindow/src/widgets/nav_goal_table_view.cpp
@@ -1,8 +1,11 @@
 #include "widgets/nav_goal_table_view.h"
+#include "algorithm.h"
+#include "logger/logger.h"
 #include <QComboBox>
 #include <QHeaderView>
 #include <QLabel>
 #include <QPushButton>
+#include <QtConcurrent>
 NavGoalTableView::NavGoalTableView(QWidget *_parent_widget)
     : QTableView(_parent_widget) {
   table_model_ = new QStandardItemModel();
@@ -38,6 +41,8 @@ void NavGoalTableView::UpdateTopologyMap(const TopologyMap &_topology_map) {
   topologyMap_ = _topology_map;
 }
 void NavGoalTableView::UpdateSelectPoint(const TopologyMap::PointInfo &point) {
+  if (!this->isEnabled())
+    return;
   QModelIndexList selectedIndexes = selectionModel()->selectedRows();
   if (selectedIndexes.size() == 1) {
     QModelIndex modelIndex = model()->index(selectedIndexes[0].row(), 0);
@@ -47,10 +52,16 @@ void NavGoalTableView::UpdateSelectPoint(const TopologyMap::PointInfo &point) {
       QComboBox *comboBox = static_cast<QComboBox *>(widget);
       comboBox->setCurrentText(point.name.c_str());
     }
+  } else {
+    QWidget *widget =
+        indexWidget(model()->index(table_model_->rowCount() - 1, 0));
+    if (widget) {
+      QComboBox *comboBox = static_cast<QComboBox *>(widget);
+      comboBox->setCurrentText(point.name.c_str());
+    }
   }
 }
 void NavGoalTableView::AddItem() {
-  std::cout << "add item" << std::endl;
 
   QComboBox *comboBox = new QComboBox();
   for (auto point : topologyMap_.points) {
@@ -73,4 +84,41 @@ void NavGoalTableView::AddItem() {
   setIndexWidget(table_model_->index(row, 1), label_status);
   setIndexWidget(table_model_->index(row, 2), button_remove);
   setIndexWidget(table_model_->index(row, 3), button_run);
+}
+void NavGoalTableView::StartTaskChain() {
+  is_task_chain_running_ = true;
+  QtConcurrent::run([this]() {
+    for (int row = 0; row < table_model_->rowCount(); ++row) {
+
+      QComboBox *comboBoxName =
+          static_cast<QComboBox *>(indexWidget(model()->index(row, 0)));
+      QLabel *label_status =
+          static_cast<QLabel *>(indexWidget(model()->index(row, 1)));
+      label_status->setText("Running");
+      TopologyMap::PointInfo point =
+          topologyMap_.GetPoint(comboBoxName->currentText().toStdString());
+      RobotPose target_pose = point.ToRobotPose();
+      emit signalSendNavGoal(target_pose);
+      RobotPose diff = absoluteDifference(target_pose, robot_pose_);
+      while (diff.mod() > 0.1 || diff.theta > deg2rad(5)) {
+        LOG_INFO("Task chain is running diff:" << diff);
+        diff = absoluteDifference(target_pose, robot_pose_);
+        QThread::msleep(100);
+      }
+      label_status->setText("Finish");
+      if (!is_task_chain_running_) {
+        emit signalTaskFinish();
+        LOG_INFO("Task chain is stopped");
+        return;
+      }
+    }
+  });
+}
+void NavGoalTableView::StopTaskChain() {
+  if (is_task_chain_running_) {
+    is_task_chain_running_ = false;
+  }
+}
+void NavGoalTableView::UpdateRobotPose(const RobotPose &pose) {
+  robot_pose_ = pose;
 }
---
+++ plugin/task_processor/task_processor.h
@@ -0,0 +1,11 @@
+#pragma once
+ 
+class TaskProcessor
+{
+private:
+    /* data */
+public:
+    TaskProcessor(/* args */);
+    ~TaskProcessor();
+};
+
