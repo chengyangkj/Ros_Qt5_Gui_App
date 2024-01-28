99b4ca75600c370807ebba9c7e57a0beadd1ceb7
chengyangkj <chengyangkj@outlook.com>
2024-01-28 11:41:19 +0800

    add yaml-cpp

--- 3rdpart/yaml-cpp/test/gtest-1.11.0/googletest/src/gtest-test-part.cc
+++ 3rdpart/yaml-cpp/test/gtest-1.11.0/googletest/src/gtest-test-part.cc
@@ -86,8 +86,8 @@ namespace internal {
 
 HasNewFatalFailureHelper::HasNewFatalFailureHelper()
     : has_new_fatal_failure_(false),
-      original_reporter_(GetUnitTestImpl()->
-                         GetTestPartResultReporterForCurrentThread()) {
+      original_reporter_(
+          GetUnitTestImpl()->GetTestPartResultReporterForCurrentThread()) {
   GetUnitTestImpl()->SetTestPartResultReporterForCurrentThread(this);
 }
 
@@ -98,8 +98,7 @@ HasNewFatalFailureHelper::~HasNewFatalFailureHelper() {
 
 void HasNewFatalFailureHelper::ReportTestPartResult(
     const TestPartResult& result) {
-  if (result.fatally_failed())
-    has_new_fatal_failure_ = true;
+  if (result.fatally_failed()) has_new_fatal_failure_ = true;
   original_reporter_->ReportTestPartResult(result);
 }
 
