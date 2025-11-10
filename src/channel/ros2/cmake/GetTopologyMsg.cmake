55fe4d63cf16bdec479ba52a4143234c83a1a3e2
chengyang <chengyangkj@outlook.com>
2025-11-10 14:42:57 +0800

    fix topology msg load bug

--- src/channel/ros2/cmake/GetTopologyMsg.cmake
+++ src/channel/ros2/cmake/GetTopologyMsg.cmake
@@ -14,6 +14,28 @@ FetchContent_Declare(
 FetchContent_GetProperties(topology_msgs)
 if(NOT topology_msgs_POPULATED)
   FetchContent_MakeAvailable(topology_msgs)
+  
+  set(TOPOLOGY_MSGS_TARGETS
+    topology_msgs__rosidl_generator_cpp
+    topology_msgs__rosidl_typesupport_cpp
+    topology_msgs__rosidl_typesupport_fastrtps_cpp
+    topology_msgs__rosidl_typesupport_introspection_cpp
+  )
+  
+  foreach(TARGET_NAME ${TOPOLOGY_MSGS_TARGETS})
+    if(TARGET ${TARGET_NAME})
+      set_target_properties(${TARGET_NAME} PROPERTIES
+        LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
+        RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
+      )
+      
+      install(TARGETS ${TARGET_NAME}
+        RUNTIME DESTINATION bin/lib
+        LIBRARY DESTINATION bin/lib
+        ARCHIVE DESTINATION bin/lib
+      )
+    endif()
+  endforeach()
 endif()
 
 # import targets:
