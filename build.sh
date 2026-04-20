ba6d9417925e1d3e7479cd3662d8ae9f0fb98920
chengyangkj <chengyangkj@qq.com>
2026-04-20 19:41:54 +0800

    fix release

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 sudo apt-get install qtbase5-private-dev libqt5svg5-dev libsdl2-dev libsdl2-image-dev -y
 cmake . -B build \
   -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX="$(pwd)/build/install" \
   $@
 cmake --build build
 cmake --install build
